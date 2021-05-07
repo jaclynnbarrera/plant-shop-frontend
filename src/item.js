eeab1751fd871c56bd140b46d3867817c1b850cc
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-05-07 11:25:11 -0400

    starts to build out singleItem function in item.js

--- src/item.js
+++ src/item.js
@@ -2,10 +2,10 @@
 function fetchItems(){
     fetch("http://localhost:3000/items")
     .then(resp => resp.json())
-    .then(appendItems)
+    .then(allItems)
 }
 
-function appendItems(items) {
+function allItems(items) {
     const itemsDiv = document.getElementById('itemsContainer')
     for (let item of items) {
        
@@ -14,22 +14,45 @@ function appendItems(items) {
         itemsDiv.appendChild(eachItemDiv)
         
         //setting up item header and appending to individ item div
-        const itemHeader = document.createElement('h4')
-        itemHeader.innerText = item.name
-        eachItemDiv.appendChild(itemHeader)
+        const itemName = document.createElement('h4')
+        itemName.innerText = item.name
+        eachItemDiv.appendChild(itemName)
 
+        //price
         const itemPrice = document.createElement('li')
         itemPrice.innerText = `$${item.price}`
         eachItemDiv.appendChild(itemPrice)
 
+        //button
         const infoButton = document.createElement("BUTTON");
         infoButton.textContent = 'See more info'
         eachItemDiv.appendChild(infoButton)
+
+        infoButton.addEventListener('click', function(e){
+            e.preventDefault
+            console.log(item)
+            //then this will head to single item function
+
+            singleItem(item)
+        })
 
     }
 }
 
 
+function singleItem (item){
+    console.log("we are in single item!")
+    //setting up containers
+    let singleItemDiv = document.createElement('div')
+    let itemName = document.createElement('h2')
+
+    let itemDescription = item.itemDescription
+    
+
+    console.log(item.description)
+
+
+}
 
 
 
